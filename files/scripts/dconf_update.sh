b9d947e8703d5a328595d3284f5f7d5631e0819f
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-01-31 16:30:53 +0700

    Create GDM config using script

--- files/scripts/dconf_update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-dconf update
---
+++ files/scripts/lontara-customize.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+mkdir -p /etc/dconf/profile
+cat <<EOF >/etc/dconf/profile/gdm
+user-db:user
+system-db:gdm
+file-db:/usr/share/gdm/greeter-dconf-defaults
+EOF
+
+mkdir -p /etc/dconf/db/gdm.d
+
+cat <<EOF >/etc/dconf/db/gdm.d/00-gdm-settings
+[org/gnome/login-screen]
+logo='/usr/share/pixmaps/lontara.svg'
+EOF
+
+dconf update
