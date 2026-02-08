56a158e6446021b8cadb556addaad9b30a7e092c
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-02-08 12:53:20 +0700

    Rebuild trivalent selinux policy

--- files/scripts/selinux.sh
+++ files/scripts/selinux.sh
@@ -2,7 +2,10 @@
 
 set -oue pipefail
 
+dnf install -y --setopt=install_weak_deps=False policycoreutils-devel
+
 # apply SELinux policies for trivalent browser
 cd ./selinux/
 make -f /usr/share/selinux/devel/Makefile trivalent.pp
 semodule -i trivalent.pp
+restorecon -FRv /usr
