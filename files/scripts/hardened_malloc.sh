a476c155c2c62a3e9d80d9c60ab6c73dd90b2bbd
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-02-18 01:38:57 +0700

    Add multimedia codecs

--- files/scripts/hardened_malloc.sh
+++ files/scripts/hardened_malloc.sh
@@ -3,3 +3,6 @@
 set -euo pipefail
 
 sed -i -e '$a\LD_PRELOAD DEFAULT="libhardened_malloc.so libno_rlimit_as.so"' -e '/^LD_PRELOAD[[:space:]]/d' /etc/security/pam_env.conf
+
+# Set ld_preload only for the root user
+chmod 600 /etc/ld.so.preload
