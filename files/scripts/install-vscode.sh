8fbb5713aa6456c0228f0a7008154a4fc92ba33e
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-02-22 23:31:32 +0700

    Test install-vscode script

--- files/scripts/install-vscode.sh
+++ files/scripts/install-vscode.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
 dnf check-update && dnf install -y code
