4d9570f7199db55f691d6749de8601a811820371
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-04-15 14:24:47 +0700

    why curl not follow redirect?

--- files/scripts/install-warp-terminal.sh
+++ files/scripts/install-warp-terminal.sh
@@ -6,9 +6,8 @@ ARCH=$(uname -m)
 if [ "$ARCH" == "x86_64" ]; then
   echo "Detected architecture: x86_64"
   echo "Installing Warp Terminal..."
-  DOWNLOAD_URL="https://app.warp.dev/get_warp?package=rpm"
 
-  curl -L "${DOWNLOAD_URL}" -o /tmp/warp-terminal.rpm
+  curl -L "https://app.warp.dev/download?package=rpm" -o /tmp/warp-terminal.rpm
 
   dnf install -y /tmp/warp-terminal.rpm
 
