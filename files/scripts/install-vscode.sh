2e188c06520db8d92edd8c2cee71cd83dbe4dd7f
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-04-20 12:37:05 +0700

    Add Ublue homebrew tap

--- files/scripts/install-vscode.sh
+++ files/scripts/install-vscode.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+brew tap ublue-os/tap
 
-dnf check-update && dnf install -y code
+brew install --cask visual-studio-code-linux
