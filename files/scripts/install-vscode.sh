7b8e22ecbff90109dc47df20342953fc2b13a957
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-04-20 19:43:26 +0700

    Disable add ublue tap script

--- files/scripts/install-vscode.sh
+++ files/scripts/install-vscode.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-brew tap ublue-os/tap
+rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
-brew install --cask visual-studio-code-linux
+dnf check-update && dnf install -y code
