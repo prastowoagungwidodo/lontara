096e7c533a9a49f7a9f07de051bf5a8f60f28282
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-07-14 09:52:56 +0700

    Fix install ubuntu icon

--- files/scripts/install-ubuntu-icon.sh
+++ files/scripts/install-ubuntu-icon.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 echo "Downloading ubuntu-mono-icons"
-curl -L -o temp.zip "https://github.com/prastowoagungwidodo/ubuntu-mono-icons/archive/refs/tags/v0.0.2.zip" && unzip temp.zip -d /usr/share/icons && rm temp.zip
+curl -L -o temp.zip "https://github.com/prastowoagungwidodo/ubuntu-mono-icons/archive/refs/tags/v0.0.2.zip" && unzip temp.zip && mv ubuntu-mono-icons-0.0.2/*  /usr/share/icons && rm temp.zip && rm -rf ubuntu-mono-icons-0.0.2
 echo "ubuntu-mono-icons installed"
 
