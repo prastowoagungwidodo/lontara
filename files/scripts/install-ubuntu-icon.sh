7b156f4870138ae0d318026ba5d4a27838e1e17a
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-07-14 13:51:47 +0700

    Fix: ubuntu icon theme installation....

--- files/scripts/install-ubuntu-icon.sh
+++ files/scripts/install-ubuntu-icon.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-echo "Downloading ubuntu-mono-icons"
-curl -L -o temp.zip "https://github.com/prastowoagungwidodo/ubuntu-mono-icons/archive/refs/tags/v0.0.2.zip" && unzip temp.zip && mv ubuntu-mono-icons-0.0.2/*  /usr/share/icons && rm temp.zip && rm -rf ubuntu-mono-icons-0.0.2
-echo "ubuntu-mono-icons installed"
+curl -L \
+  https://github.com/prastowoagungwidodo/ubuntu-mono-icons/archive/refs/tags/v0.0.2.zip \
+  -o /tmp/ubuntu-mono-icons.zip
+
+unzip -q /tmp/ubuntu-mono-icons.zip -d /tmp
+
+cp -a /tmp/ubuntu-mono-icons-*/* /usr/share/icons/
 
