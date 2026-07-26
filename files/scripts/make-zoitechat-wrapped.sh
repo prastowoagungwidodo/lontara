c842b3de0a6ff97270985a61a8f435745950b512
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-07-26 17:45:29 +0700

    Salah comment :D

--- files/scripts/make-zoitechat-wrapped.sh
+++ files/scripts/make-zoitechat-wrapped.sh
@@ -5,9 +5,6 @@ set -oue pipefail
 echo "Make Zoitechat executable wrapped using bwrap"
 echo "Patch desktop file to use the wrapper script"
 
-// Wrapper script located at /usr/bin/zoitechat-wrapper
-// Change Exec line in desktop file to point to the wrapper script
-
 chmod +x /usr/bin/zoitechat-wrapper
 sed -i 's|Exec=zoitechat|Exec=/usr/bin/zoitechat-wrapper|g' /usr/share/applications/zoitechat.desktop
 
